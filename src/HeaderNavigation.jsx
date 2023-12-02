99a9fca853d74670e9a45002ccd669996418f062
Augustus Brennan <augustusmb@gmail.com>
2023-12-01 16:14:18 -0800

    Update header color

--- src/HeaderNavigation.jsx
+++ src/HeaderNavigation.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 
 const HeaderNavigation = () => {
   return (
-    <div className="border-solid border-2 border-slate-500 grid grid-cols-3">
+    <div className="border-solid border-2 border-slate-300 grid grid-cols-3 bg-slate-300">
       <div>
         <Link to="/">
           <img src={eTrainingIcon} className="h-14 sm:h-20" />
