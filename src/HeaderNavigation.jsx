d1e617a1937c20e59c38666f3ece6cec7745dad9
Augustus Brennan <augustusmb@gmail.com>
2024-01-12 01:12:37 -0800

    Update HeaderNavigation styling

--- src/HeaderNavigation.jsx
+++ src/HeaderNavigation.jsx
@@ -11,7 +11,7 @@ const HeaderNavigation = () => {
           <img src={eTrainingIcon} className="h-14 sm:h-20" />
         </Link>
       </div>
-      <div className="col-span-2 flex flex-wrap items-center justify-between">
+      <div className="col-span-2 flex flex-wrap items-center justify-between px-1">
         <Link
           to="/"
           className="w-18 rounded bg-indigo-700 px-3 py-2 text-sm font-bold text-slate-050 hover:bg-indigo-600 lg:w-28 lg:text-lg"
