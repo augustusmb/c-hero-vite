25eaa23fb47fb268848893cba6b9099833bb2b97
Augustus Brennan <augustusmb@gmail.com>
2024-01-25 10:31:33 -0800

    fix the routes server path

--- server/server.js
+++ server/server.js
@@ -30,25 +30,25 @@ function getKey(header, callback){
   });
 }
 
-// const checkJwt = (req, res, next) => {
-//   const authHeader = req.headers.authorization;
+const checkJwt = (req, res, next) => {
+  const authHeader = req.headers.authorization;
 
-//   if (authHeader) {
-//     const token = authHeader.split(' ')[1];
+  if (authHeader) {
+    const token = authHeader.split(' ')[1];
 
-//     jwt.verify(token, getKey, { algorithms: ['RS256'] }, (err, user) => {
-//       if (err) {
-//         console.log('err: ', err)
-//         return res.status(403).send(err.message);
-//       }
+    jwt.verify(token, getKey, { algorithms: ['RS256'] }, (err, user) => {
+      if (err) {
+        console.log('err: ', err)
+        return res.status(403).send(err.message);
+      }
 
-//       req.user = user;
-//       next();
-//     });
-//   } else {
-//     res.sendStatus(401);
-//   }
-// };
+      req.user = user;
+      next();
+    });
+  } else {
+    res.sendStatus(401);
+  }
+};
 
 app.use((req, res, next) => {
   console.log(`Received ${req.method} request for ${req.url}`);
