12b9759b2a1e785daab2c547516a3547c6529f73
Augustus Brennan <augustusmb@gmail.com>
2024-02-08 13:04:50 -0800

    Add fallback route to serve index.html

--- server/server.js
+++ server/server.js
@@ -74,6 +74,10 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, "../dist/index.html"));
 });
 
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
+});
+
 app.listen(port, () => {
   console.log('listening on port: ', port)
 })
