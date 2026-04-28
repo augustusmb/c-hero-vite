26fc1f9d0b401a0bbc18b6eff1e74cd78d099a05
Augustus Brennan <augustusmb@gmail.com>
2026-04-27 20:49:41 -0700

    Sort company, vessel, and port names in fetchFormOptions SQL query

--- db/queries/users/fetchFormOptions.sql
+++ db/queries/users/fetchFormOptions.sql
@@ -1,4 +1,4 @@
-SELECT 
-    (SELECT json_agg(json_build_object('id', id, 'name', name)) FROM companies) as companies,
-    (SELECT json_agg(json_build_object('id', id, 'name', name)) FROM vessels) as vessels,
-    (SELECT json_agg(json_build_object('id', id, 'name', name)) FROM ports) as ports;
+SELECT
+    (SELECT json_agg(json_build_object('id', id, 'name', name) ORDER BY lower(name)) FROM companies) as companies,
+    (SELECT json_agg(json_build_object('id', id, 'name', name) ORDER BY lower(name)) FROM vessels) as vessels,
+    (SELECT json_agg(json_build_object('id', id, 'name', name) ORDER BY lower(name)) FROM ports) as ports;
